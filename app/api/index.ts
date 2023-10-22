c635260aec68fd7b0b1f1474568ab4168e5798d7
Antanas Stanys <antanas@stanys.eu>
2023-10-22 08:39:09 +0300

    Deleted console.logs

--- app/api/index.ts
+++ app/api/index.ts
@@ -5,38 +5,33 @@ import {appendFileSync} from "fs";
 import {join} from "path";
 
 export class Api {
-    private readonly url = "https://yesno.wtf/api";
+  private readonly url = "https://yesno.wtf/api";
 
-    constructor(public repeat: number) {
+  constructor(public repeat: number) {
+  }
+
+  public async sendHttpRequests(repeat?: number): Promise<void> {
+    const repeatCount = repeat || this.repeat;
+    const promises = new Array(repeatCount).fill(0).map(async (): Promise<EAnswer> => {
+      const response: IApiResponse = await sendHttpRequest({url: this.url})
+
+      return response.answer;
+    });
+
+    const answers = await Promise.all(promises);
+
+    return this.handleAnswers(answers);
+  }
+
+  private async handleAnswers(answers: EAnswer[]): Promise<void> {
+    const yesAnswersCount = answers.filter((answer: EAnswer): boolean => answer === EAnswer.YES).length;
+
+    appendFileSync(join(__dirname, '../../', "output/log.txt"), yesAnswersCount.toString());
+
+    if (!yesAnswersCount) {
+      return;
     }
 
-    public async sendHttpRequests(repeat?: number): Promise<void> {
-        if (!repeat) {
-            console.log('initial requests');
-        } else {
-            console.log('repeat requests');
-        }
-        const repeatCount = repeat || this.repeat;
-        const promises = new Array(repeatCount).fill(0).map(async (): Promise<EAnswer> => {
-            const response: IApiResponse = await sendHttpRequest({url: this.url})
-
-            return response.answer;
-        });
-
-        const answers = await Promise.all(promises);
-
-        return this.handleAnswers(answers);
-    }
-
-    private async handleAnswers(answers: EAnswer[]): Promise<void> {
-        const yesAnswersCount = answers.filter((answer: EAnswer): boolean => answer === EAnswer.YES).length;
-        console.log('yesAnswersCount', yesAnswersCount);
-        appendFileSync(join(__dirname, '../../', "output/log.txt"), yesAnswersCount.toString());
-
-        if (!yesAnswersCount) {
-            return;
-        }
-
-        await this.sendHttpRequests(yesAnswersCount);
-    }
+    await this.sendHttpRequests(yesAnswersCount);
+  }
 }
